1a73dcc3ae6ebcae0d54cb471dd491437fbd3e97
Chetan Pratap Patil <149082202+ChetanPatil0@users.noreply.github.com>
2026-09-23 18:01:05 +0530

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -54,14 +54,14 @@ export const authOptions: NextAuthOptions = {
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
-        token.role = (user as { role: string }).role;
+        token.role = (user as { role: "CUSTOMER" | "ADMIN" }).role;
       }
       return token;
     },
     async session({ session, token }) {
       if (session.user) {
-        (session.user as { id: string }).id = token.id as string;
-        (session.user as { role: string }).role = token.role as string;
+        session.user.id = token.id as string;
+        session.user.role = token.role;
       }
       return session;
     },
