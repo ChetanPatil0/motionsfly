0af5d3bd4f12835ac9b5bf35f45a00492854da93
Chetan Pratap Patil <149082202+ChetanPatil0@users.noreply.github.com>
2026-09-24 12:05:24 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   output: "standalone",
-
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" }
     ]
   },
-
   experimental: {
-    serverActions: {
-      bodySizeLimit: "10mb"
-    },
-
-    serverComponentsExternalPackages: ["argon2"]
+    serverActions: { bodySizeLimit: "10mb" },
+    // 1. Tell Next.js not to bundle argon2 through Webpack
+    serverComponentsExternalPackages: ["argon2"],
+    // 2. Tell Vercel to copy the native Linux C++ binaries into the lambda deployment
+    outputFileTracingIncludes: {
+      "/**": ["./node_modules/argon2/prebuilds/**/*"]
+    }
   }
 };
 
